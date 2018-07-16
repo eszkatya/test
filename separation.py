d4ff3340cb902dddda83debd0bb481a29a2eb4ba
eszkatya <39097863+eszkatya@users.noreply.github.com>
2018-07-16 22:09:24 +0200

    Update separation.py

--- separation.py
+++ separation.py
@@ -1,3 +1,11 @@
+"""This time we learn two methods to split or merge string:split() and concat(). also learn a good friend of the split() method: join(). It is an Array method. Their usage:
+
+stringObject.split(separator,howmany)
+stringObject.concat(string1,string2,...,stringx)
+arrayObject.join(separator)"""
+
+
+
 def split_and_merge(string, sp):
     separator = sp
     return separator.join(string)
