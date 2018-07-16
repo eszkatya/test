bfab29afec286af11070c98e0d447c1a0863acca
eszkatya <39097863+eszkatya@users.noreply.github.com>
2018-07-16 16:40:27 +0200

    Update correcting.py

--- correcting.py
+++ correcting.py
@@ -2,3 +2,11 @@ def correct(string):
     return string.replace("1", "I").replace("5","S").replace("0","O")
 
 print(correct("DUBL1N"))
+
+#def correct(string):
+#    return string.translate(str.maketrans("501", "SOI"))
+# string.translate és str.maketran utánanézni
+
+#def correct(string):
+#    return string.replace('1','I').replace('0','O').replace('5','S')
+# string.replacenek utánanázni
