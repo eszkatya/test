330af12610f0109a0565b7efb23b753d14d3ceb3
eszkatya <39097863+eszkatya@users.noreply.github.com>
2018-07-17 16:53:47 +0200

    Update boolean.py

--- boolean.py
+++ boolean.py
@@ -3,3 +3,9 @@ def bool_to_word(boolean):
         return 'Yes'
     else:
         return 'No'
+    
+"""ezt is lehetett vna egy sorba, ami még érdekes:
+def bool_to_word(bool):
+    return ['No', 'Yes'][bool]
+nem teljesen értem, hogy ez h működik
+ 
