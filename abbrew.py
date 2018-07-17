f8b151c144ba1fa5b15186af3b3eb9cd45a813a0
eszkatya <39097863+eszkatya@users.noreply.github.com>
2018-07-17 17:11:37 +0200

    Update abbrew.py

--- abbrew.py
+++ abbrew.py
@@ -5,6 +5,16 @@ def abbrevName(name):
     last = splitelt[1]
     print (last)
     monogram = first[0] + '.' + last[0]
-    return monogram
+    return monogram.upper
 
 print((abbrevName("Sam Harris")))
+
+#def abbrevName(name):
+#    first, last = name.upper().split(' ')
+#   return first[0] + '.' + last[0]
+
+# first és last definiálását nem értem...hogy hogy vesszővel elválasztjuk?
+
+# ez tetszik:
+#def bool_to_word(bool):
+#return "Yes" if bool else "No"
