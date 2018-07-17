20a5398924a4922f192c296b77dff9544647ebe4
eszkatya <39097863+eszkatya@users.noreply.github.com>
2018-07-17 19:36:31 +0200

    Update return divisibility.py

--- return divisibility.py
+++ return divisibility.py
@@ -1,5 +1,12 @@
+#Create a function isDivisible(n, x, y) that checks if a number n is divisible by two numbers x AND y. 
+#All inputs are positive, non-zero digits.
+
 def is_divisible(n,x,y):
     if n % x == 0 and n % y == 0:
         return True
     else:
         return False
+"""def is_divisible(n,x,y):
+    return n % x == 0 and n % y == 0
+    
+    itt nem volt nagy változatosság, vagy ehhez nagyon hasonló kódok..vagy lambdás megoldások
