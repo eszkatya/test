9a75bd96e8d486b9f9c57df6864f09d18312d9a3
eszkatya <39097863+eszkatya@users.noreply.github.com>
2018-07-17 19:27:21 +0200

    Update abbrew.py

--- abbrew.py
+++ abbrew.py
@@ -1,3 +1,9 @@
+#Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
+#The output should be two capital letters with a dot seperating them.
+#It should look like this:
+#Sam Harris => S.H
+#Patrick Feeney => P.F
+
 def abbrevName(name):
     splitelt = name.split(" ")
     first = splitelt[0]
