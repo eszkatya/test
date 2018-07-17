333a22102cb175160ea47b44d895cd09a70cae81
eszkatya <39097863+eszkatya@users.noreply.github.com>
2018-07-17 16:48:20 +0200

    Update evenorodd.py

--- evenorodd.py
+++ evenorodd.py
@@ -5,3 +5,8 @@ def even_or_odd(number):
         return "Odd"
 
 print(even_or_odd(0))
+
+# ezt is lehetett volna egy sorba
+#def even_or_odd(number):
+#  return 'Odd' if number % 2 else 'Even'
+#(a kódjaim felére igaz ez)
