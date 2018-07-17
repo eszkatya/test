68d9419a86b03f0879e918411dd5e189044bd07b
eszkatya <39097863+eszkatya@users.noreply.github.com>
2018-07-17 16:45:41 +0200

    Update dishandbeast.py

--- dishandbeast.py
+++ dishandbeast.py
@@ -5,3 +5,13 @@ def feast(beast, dish):
        return "False"
 
 print (feast("great blue heron", "nyamnyam"))
+
+
+
+#def feast(beast, dish):
+#    return beast.startswith(dish[0]) and beast.endswith(dish[-1])
+#startswith és endswith újak.
+
+#def feast(beast, dish):
+#    return beast[0] == dish[0] and beast[-1] == dish[-1]    
+# fölösleges volt az enyém iffel
