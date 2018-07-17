9a212c51391727cf7e3ee6950958fe5f6033f4f2
eszkatya <39097863+eszkatya@users.noreply.github.com>
2018-07-17 19:13:19 +0200

    Update needle.py

--- needle.py
+++ needle.py
@@ -1,7 +1,14 @@
 def find_needle(haystack):
+    helyezes = []
     if "needle" in haystack:
        helyezes = haystack.index("needle")
-       #return type(helyezes)
-       return "found the needle at position" + " "+str(helyezes)
+       return "found the needle at position " + str(helyezes)
 
 print(find_needle(['3', '123124234', None, 'needle', 'world', 'hay', 2, '3', True, False]))
+
+"""igazából ez tetszik, mert hasonlít az enyémre:
+def find_needle(haystack):
+    return "found the needle at position " + str(haystack.index("needle"))
+    
+a százalékjek ilyen használatát meg jó lenne végre megértenem
+def find_needle(haystack): return 'found the needle at position %d' % haystack.index('needle')
