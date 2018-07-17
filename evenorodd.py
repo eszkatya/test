431b125e56eea7a320fb7e0d03dabc4e55a1eeae
eszkatya <39097863+eszkatya@users.noreply.github.com>
2018-07-17 19:31:48 +0200

    Update evenorodd.py

--- evenorodd.py
+++ evenorodd.py
@@ -1,3 +1,5 @@
+#Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
 def even_or_odd(number):
     if number % 2 == 0:
         return "Even"
@@ -6,7 +8,7 @@ def even_or_odd(number):
 
 print(even_or_odd(0))
 
-# ezt is lehetett volna egy sorba
-#def even_or_odd(number):
-#  return 'Odd' if number % 2 else 'Even'
-#(a kódjaim felére igaz ez)
+"""ezt is lehetett volna, sokkal szebb
+def even_or_odd(number):
+  return 'Odd' if number % 2 else 'Even'
+(a kódjaim felére igaz ez)
